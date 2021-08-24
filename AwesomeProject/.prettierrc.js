d8c0f9e85443e3cb5aafd97366af5e37b49974fb
Dan.BlessettWilliams <Dan.BlessettWilliams@axapppuk.onmicrosoft.com>
2021-08-24 18:53:01 +0100

    prettier setup

--- AwesomeProject/.prettierrc.js
+++ AwesomeProject/.prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  overrides: [
+    {
+      files: '*.yaml',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
