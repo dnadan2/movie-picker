a7374c2a52fa53144efedbb8d1fe8fee2c682d04
Dan.BlessettWilliams <Dan.BlessettWilliams@axapppuk.onmicrosoft.com>
2021-08-24 19:59:53 +0100

    refactor and ts

---
+++ AwesomeProject/app/App.test.tsx
@@ -0,0 +1,5 @@
+import App from './App';
+
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
---
+++ AwesomeProject/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ backgroundColor: 'red' }}>
+        <Text>some text</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
