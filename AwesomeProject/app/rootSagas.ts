a80868bdd096b60e3d8fc1da50ab51fddd628114
Ethan Ely <eely@worth.systems>
2021-11-24 16:27:22 +0200

    Root saga fires off getFilmAvailabilitySaga

--- AwesomeProject/app/rootSagas.ts
+++ AwesomeProject/app/rootSagas.ts
@@ -3,7 +3,7 @@ import { all } from 'redux-saga/effects';
 import { getFilmAvailabilitySaga } from './sagas/streamingAvailability/get-film-availability-sagas';
 
 function* rootSaga() {
-  yield all([getFilmAvailabilitySaga]);
+  yield all([getFilmAvailabilitySaga()]);
 }
 
 export const registerSagas = (sagaMiddleware: SagaMiddleware) => {
