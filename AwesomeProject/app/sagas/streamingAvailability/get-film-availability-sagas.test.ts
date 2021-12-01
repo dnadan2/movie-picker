262142adc4e7e36b486e3d9c6b91abb622515416
AJ Greyling <agreyling@worth.systems>
2021-12-01 16:18:50 +0200

    added mock film

--- AwesomeProject/app/sagas/streamingAvailability/get-film-availability-sagas.test.ts
+++ AwesomeProject/app/sagas/streamingAvailability/get-film-availability-sagas.test.ts
@@ -2,13 +2,15 @@ import { expectSaga } from 'redux-saga-test-plan';
 import { onGetStreamingAvailability } from '../../actions/streamingAvailabilityAction';
 import { getFilmAvailabilitySaga } from './get-film-availability-sagas';
 import * as streamingAvailability from '../../api/StreamingAvailability/StreamingAvailability';
-import { StreamingAvailabilityResponse } from '../../Api/StreamingAvailability/interfaces';
+import { RecursivePartial } from '../../types';
+import { StreamingAvailabilityItems } from '../../Api/StreamingAvailability/interfaces';
+import { StreamingAvailabilitySelector } from '../../redux/streamingAvailability/streamingAvailabilitySelector';
 
 describe('Saga: get film availability', () => {
   test('getFilmAvailabilitySaga', () => {
-    const expectedResult: Partial<StreamingAvailabilityResponse> = {
-      posterURLs: { original: '' },
-    };
+    const expectedResult: RecursivePartial<StreamingAvailabilityItems[]> = [
+      { posterURLs: { original: '' } },
+    ];
 
     const mockGetAvailability = jest.fn().mockReturnValue(expectedResult);
 
@@ -19,13 +21,43 @@ describe('Saga: get film availability', () => {
     return expectSaga(getFilmAvailabilitySaga)
       .put(
         onGetStreamingAvailability.success(
-          expectedResult as StreamingAvailabilityResponse
+          expectedResult as StreamingAvailabilityItems[]
         )
       )
       .dispatch(onGetStreamingAvailability.request())
       .run({ silenceTimeout: true })
       .then(() => {
         expect(mockGetAvailability).toHaveBeenCalled();
+      });
+  });
+
+  test("don't call API if results exist in store", () => {
+    const mockGetAvailability = jest.fn();
+
+    jest
+      .spyOn(streamingAvailability, 'getAvailability')
+      .mockImplementation(mockGetAvailability);
+
+    const mockStoredFilms: RecursivePartial<StreamingAvailabilityItems[]> = [
+      { title: 'Space Odesey' },
+    ];
+    const mockGetAvailableFilms = jest
+      .fn()
+      .mockReturnValue({ mockStoredFilms });
+    jest
+      .spyOn(StreamingAvailabilitySelector, 'getAvailableFilms')
+      .mockImplementation(mockGetAvailableFilms);
+
+    return expectSaga(getFilmAvailabilitySaga)
+      .put(
+        onGetStreamingAvailability.success(
+          mockStoredFilms as StreamingAvailabilityItems[]
+        )
+      )
+      .dispatch(onGetStreamingAvailability.request())
+      .run({ silenceTimeout: true })
+      .then(() => {
+        expect(mockGetAvailability).not.toHaveBeenCalled();
       });
   });
 });
